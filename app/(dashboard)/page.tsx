9fb0906714aab33cd2181c445b6614efa97e23ef
Rodrigo Bezerra <3rd.rodrigo.bezerra@spcgrafeno.com.br>
2026-01-07 08:13:35 -0300

    Add Prettier configuration and plugins; restructure app layout and components

--- app/(dashboard)/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Hom() {
-  return (
-    <div>
-      <h1 className="flex justify-center items-center min-h-screen text-2xl font-semibold">
-        Hello World!
-      </h1>
-    </div>
-  );
-}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Hom() {
+  return (
+    <div className="py-4">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
